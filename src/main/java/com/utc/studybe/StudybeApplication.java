6dec78ff648416d3a276e151129f2bb1d56c8fe6
Main <main.brightsoft@gmail.com>
2024-11-09 09:29:44 +0700

    Initial

---
+++ src/main/java/com/utc/studybe/StudybeApplication.java
@@ -0,0 +1,13 @@
+package com.utc.studybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudybeApplication.class, args);
+	}
+
+}
